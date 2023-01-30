4b1a114c64c1cf5579fc67d3c64e0306dd43b8e7
Akseli <74552940+denton310@users.noreply.github.com>
2023-01-30 12:29:31 +0200

    Delete vk5 directory

--- vk5/es3.js
+++
@@ -1,9 +0,0 @@
-
-function sayHello(name) {
-    const greeting = `Hello, ${name}`;
-    const helloToConsole = function() { console.log(greeting);} 
-    return helloToConsole;
- }
-
-const hi = sayHello('Akseli');
-hi();
